ca3215b8043cba6fa06534e02a83af27b63d0050
Miguel Ángel López Vicente <miguel.angel.lopez.vicente@gmail.com>
2015-09-02 15:26:20 +0200

    Update discourse-dev-nav-item.js

--- assets/javascripts/discourse-dev-nav-item.js
+++ assets/javascripts/discourse-dev-nav-item.js
@@ -1,3 +1,3 @@
 (function() {
-     document.querySelector('.nav.nav-pills').innerHTML += "<li title=\"Calendario de Actividades\" class=\"ember-view\"><a href=\"http://almeriaenfriki.es/t/calendario-de-eventos-actividades/183\">Calendario</a></li>";
+     Discourse.HTML.setCustomHTML('extraNavItem', "<li title=\"Calendario de Actividades\" class=\"ember-view\"><a href=\"http://almeriaenfriki.es/t/calendario-de-eventos-actividades/183\">Calendario</a></li>");
 })();
